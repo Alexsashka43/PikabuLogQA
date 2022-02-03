d25f7780818374176b83c5100389a7208d1953c4
Alexsashka43 <63096544+Alexsashka43@users.noreply.github.com>
2022-02-03 22:45:38 +0300

    ver 1.2

--- test.py
+++ test.py
@@ -7,40 +7,41 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class LogInTest(unittest.TestCase):
+
     url = 'https://pikabu.ru/'
-    user_nick = 'qwertypitec'
-    user_password = '89530poneg4,.3'
     driver = webdriver.Chrome()
     driver.get(url)
-    driver.implicitly_wait(30)
-
-
+    driver.implicitly_wait(10)
+    user_nick = 'qwertypitec'
+    user_password = '89530poneg4,.3'
     e_mail = driver.find_element(By.NAME, 'username')
     password = driver.find_element(By.NAME, 'password')
     main_menu = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[2]/div/div[2]/div[1]/div')
-    button = driver.find_element(By.XPATH, '//*[@id="signin-form"]/div[7]')
+    button_entrance = driver.find_element(By.XPATH, '//*[@id="signin-form"]/div[7]')
 
-
-    """Позитивный тест"""
+    """Positive Test"""
     def test_positive(self):
         self.e_mail.send_keys(self.user_nick)
         self.password.send_keys(self.user_password + Keys.RETURN)
-        self.assertEqual(self.driver.find_element(By.LINK_TEXT, self.user_nick), self.user_nick, 'Log in')
-        self.assertIsNotNone(self.driver.find_element(By.ID, 'rc-imageselect'), 'Capcha')
+        self.assertEqual(self.driver.find_element(By.CLASS_NAME, 'user__info-item').text, self.user_nick)
+        button_exit = self.driver.find_element(By.CLASS_NAME, 'user__info-item user__exit')
+        button_exit.click()
+        # self.assertIsNotNone(self.driver.find_element(By.ID, 'rc-imageselect'), 'Capcha')
 
-
-    """Негативные тесты"""
+    """Negative Tests"""
     def test_wrong_fields(self):
         self.e_mail.send_keys("wrong_name")
         self.password.send_keys("wrong_password" + Keys.RETURN)
-        self.assertIsNotNone(self.driver.find_element(By.CLASS_NAME, 'auth__error'), 'Log in')
-        self.main_menu.click()
-
+        self.assertIsNotNone(self.driver.find_element(By.CLASS_NAME, 'auth__error'))
 
     def test_empty_fields(self):
-        self.button.click()
+        self.button_entrance.click()
         self.assertIsNotNone(self.driver.find_element(By.CLASS_NAME, 'popup__hint'))
-        self.main_menu.click()
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
 
-    # driver.close()
+if __name__ == '__main__':
+    unittest.main()
+
